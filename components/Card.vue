e71859c3cabd86257fdf0e9eb53a8d3c0a4ff072
billxu <billxu0521@gmail.com>
2020-08-17 12:06:50 +0800

    no message

---
+++ components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div v-for="card in cardlist.rows" :key="card" class="lesson">
+
+      <p>{{card.name}}</p>
+
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import CardList from '../card_data.json';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      cardlist:CardList,
+    }
+  },
+  watch: {
+   
+  },
+  mounted () {
+    console.log(this.cardlist)
+  },
+  methods: {
+    
+  }
+
+}
+</script>
+
