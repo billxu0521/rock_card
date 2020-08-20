a134f624de6ad94532212cee542db4ac06256c67
billxu <billxu0521@gmail.com>
2020-08-20 11:10:43 +0800

    更新版本
    Signed-off-by: billxu <billxu0521@gmail.com>

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,42 +1,47 @@
 <template>
-  
-  <div id="app">
     <el-container>
       <el-main>
-      已持有手牌(點擊可以取消選擇)：
-      <el-row  type="flex" justify="center" class="row-bg selectcardcontainer" :gutter="10">
+      <div id="selectcardcontainer">
+      <div id="selectcardcontainertext">已持有手牌(點擊可以取消選擇)：</div>
+      <el-row  type="flex" justify="center" class="row-bg selectcardcontainer" :gutter="20" >
           <el-col  :span="6" v-for="(select_card,index) in select" :key="index"  class="selectcard">
-            <el-card @click.native="removeCard(index)" :value="index" shadow="always"> {{select_card}}</el-card>
+            <el-card :class="cardlist[select_card]" @click.native="removeCard(index)" :value="index" shadow="always"> {{select_card}}</el-card>
         </el-col>
       </el-row>
-        <transition name="score" ><p id="score" v-show="scoreshow">結果：{{score}}分</p></transition>
+        <transition name="score" ><span id="score" v-show="scoreshow">結果：{{score}}分</span></transition>
+      </div>
+      <div id="cardcontainer">
+      <el-button  @click="removeAllCard" size="mini">清除所有持有手牌</el-button>
       <br>
-      手牌選擇區：
+      <br>
+      <div id="cardcontainertext">手牌選擇區：</div>
       <el-row type="flex" class="row-bg cardcontainer" :gutter="1">
-        <el-col :span="12" class="allcard" v-for="card in allCardData.rows" :key="card.name"  >
-            <el-button  @click="addCard(card.name)" :value="card.name" round>{{card.name}}</el-button>
+        <el-col :span="12" class="allcard" v-for="card in allcarddata.rows" :key="card.name"  >
+            <el-button  :type="card.color"  @click="addCard(card.name)" :value="card.name" round>{{card.name}}</el-button>
           
         </el-col>
+        <el-col>Powered by: <a href="mailto:billxu0521@gmail.com">橫隔膜</a> </el-col>
       </el-row>
+    </div>
     </el-main>
+
   </el-container>
-  </div>
 </template>
 
 <script>
-import CardList from '../card_data.json';
+import CardList from '../card_data_cal.json';
 
 export default {
   name: 'app',
   data () {
     return {
-      allCardData:CardList,
+      allcarddata:CardList,
       cardlist:[],
       select:[],
-      card_combine:[],
+      cardcombine:[],
       score:999,
       scoreshow:false,
-      noscoreshow:true
+      bg1:'bg1.jpg'
     }
   },
   watch: {
@@ -44,9 +49,13 @@ export default {
       this.scoreshow = false
       this.noscoreshow = false
       console.log('新增卡片')
-      //this.checkCombine()
+      let score = this.checkCombine()
+      this.score = score
+      this.scoreshow = true
+      /*
       let checkscore = this.getRandom(0,10)
       if(this.select.length > 1 && checkscore > 3){
+        //this.checkCombine()
         this.score = this.getRandom(0,999)
         this.scoreshow = true
         this.noscoreshow = false
@@ -54,81 +63,81 @@ export default {
         this.scoreshow = false
         this.noscoreshow = true
       }
-      
+      */
     }
-   
   },
   mounted () {
-    console.log(this.allCardData)
-    for(let i in this.allCardData['rows']){
+    console.log(this.allcarddata['rows'])
+    let _combine = []
+    for(let i in this.allcarddata['rows']){
       //console.log(this.cardlist['rows'][i])
-      let card_combine = this.allCardData['rows'][i]
+      let card_combine = this.allcarddata['rows'][i]
       let card = card_combine['name']
-      this.cardlist.push(card)
-      let _c = []
-      //console.log(card_combine['name'])
-      if(card_combine['name'] !== ''){
-        _c.push({'card':card_combine['name'],'score':card_combine['score']})
-      }
-      if(card_combine['name2'] !== ''){
-        _c.push({'card':card_combine['name2'],'score':card_combine['score2']})
-      }
-      if(card_combine['name3'] !== ''){
-        _c.push({'card':card_combine['name3'],'score':card_combine['score3']})
-      }
-      if(card_combine['name4'] !== ''){
-        _c.push({'card':card_combine['name3'],'score':card_combine['score4']})
-      }
-      this.card_combine.push(_c)
+      this.cardlist[card] = card_combine['color']
+      _combine[card] = {'ref':this.allcarddata['rows'][i]['ref'],'score':this.allcarddata['rows'][i]['score']}
     }
-    //console.log(this.card_combine)
-    //console.log(this.cardlist)
+    this.cardcombine = _combine
+    console.log(this.cardlist)
   },
   methods: {
     addCard(value){
-      
       if(this.select.length < 7){
         this.select.push(value)
       }
     },
     removeCard(value){
-      
       if(this.select !== null){
         (this.select).splice(value, 1)
       }
     },
     getRandom(min,max){
-      return Math.floor(Math.random()*(max-min+1))+min;
+      return Math.floor(Math.random()*(max-min+1))+min
+    },
+    removeAllCard(){
+      this.select = []
     },
     checkCombine(){
-      if(this.select.length > 1){
-        for(let i in this.select){
-          console.log(this.select[i])
-          //let check_combine = []
-          //檢查組合
-          //let current_combine = []
-          //let current_combine = []
-          for(let a in this.card_combine){
-            let _c = this.card_combine[a] //每組組合
-            
-            for(let x in _c){
-              console.log(_c[x].card)
-              
+      let checkedItems = this.select
+      let score_cal = 0
+      let allcardcombine = this.cardcombine
+      for(let i in checkedItems){
+        //console.log(checkedItems[i])
+        let cardname = checkedItems[i]
+        let _cardcombine = allcardcombine[cardname]
+        let num = parseInt(allcardcombine[cardname]['score'])
+        score_cal += num
+        console.log(i+"/卡片: " + cardname + " 得分:" + num)
+        let picked_item = [parseInt(i)]
+        console.log(picked_item)
+        for(let a in _cardcombine['ref']){
+          let _ref = _cardcombine['ref'][a]
+          for(let x = 0 ; x < checkedItems.length ; ++x) {
+            if (picked_item.indexOf(x) > -1) continue;
+            if(_ref['name'] === checkedItems[x]) {
+              let _ref_num = parseInt(_ref['score'])
+              score_cal += _ref_num
+              picked_item.push(x)
+              console.log(i+"/參考卡片: " + _ref['name'] + " 得分:" + _ref_num)
+              break
             }
-            //如果check_combine > 2 才會算分
-            
           }
-
-
         }
       }
+    console.log(score_cal)
+    //this.score = score_cal
+    return score_cal
     }
+
   }
 
 }
 </script>
 
 <style>
+  body{
+    margin:0px;
+  }
+
   .el-header, .el-footer {
     background-color: #B3C0D1;
     color: #333;
@@ -148,27 +157,43 @@ export default {
     color: #333;
     text-align: center;
     line-height: 16px;
+    padding: 0px !important; 
+    height: 100vh;
   }
 
   body > .el-container {
     margin-bottom: 40px;
+    height: 100vh;
   }
   
   .selectcardcontainer{
     flex-wrap: wrap;    
     max-width: 90vw;
-    min-height: 10vh;
-    margin-bottom: 40px;
+    height: 35vh;
+  }
+
+  #selectcardcontainer{   
+    background: url('~@/../src/assets/bg1.jpg') center ;
+    background-size: cover;
+    padding-bottom: 10px;
+    min-height: 45%;
+    padding-top: 10px;
   }
 
   .selectcard{
-    margin-top: 10px;
+    margin-top: 5px;
   }
 
   .cardcontainer{
     overflow: auto;
-    flex-wrap: wrap;    
-    max-height: 60vh;
+    flex-wrap: wrap;  
+    max-height: 43vh;  
+  }
+
+  #cardcontainer{
+    background: url('~@/../src/assets/bg2.jpg') ;
+    background-size: cover;
+
   }
 
   .allcard{
@@ -190,6 +215,36 @@ export default {
 
   .selectcard button{
     font-size: 28px !important; 
+  }
+
+  #selectcardcontainertext{
+    font-weight:bold;
+    font-size: 24px;
+    -webkit-text-stroke: 1px white;
+    color: black;
+    margin-bottom: 10px;
+  }
+
+  #cardcontainertext {
+    font-weight:bold;
+    font-size: 24px;
+    -webkit-text-stroke: 1px white;
+    color: black;
+    margin-bottom: 10px;
+  }
+
+  .el-card{
+    font-size: 18px;
+  }
+
+  .el-card.danger{
+    background:#f78989;
+    color: white;
+  }
+
+  .el-card.primary{
+    background:#409EFF;
+    color: white;
   }
 
   .score-enter-active  {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/rock_card/'
-    : '/'
+	publicPath: process.env.NODE_ENV === 'production'
+	    ? '/rock_card/'
+	    : '/',
+	filenameHashing: false
+
 }
